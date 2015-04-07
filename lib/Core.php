98892640fd5829c7ba3722c94b248b6fe9423563
Alfonso Perez <alfon@ratslap.com>
2015-04-07 20:50:45 +0200

    remove unused PDO stuff

--- lib/Core.php
+++ lib/Core.php
@@ -3,24 +3,13 @@
 namespace lib;
 
 use lib\Config;
-use PDO;
 
 class Core {
-    public $dbh; // handle of the db connexion
+
     private static $instance;
 
     private function __construct() {
-        // building data source name from config
-        $dsn = 'mysql:host=' . Config::read('db.host') .
-               ';dbname='    . Config::read('db.basename') .
-               ';port='      . Config::read('db.port') .
-               ';connect_timeout=15';
-        // getting DB user from config                
-        $user = Config::read('db.user');
-        // getting DB password from config                
-        $password = Config::read('db.password');
 
-        $this->dbh = new PDO($dsn, $user, $password);
     }
 
     public static function getInstance() {
--- models/Form.php
+++ models/Form.php
@@ -4,5 +4,18 @@ namespace models;
 
 class Form extends \Illuminate\Database\Eloquent\Model {
 
+    protected $table = 'forms';
+
+    public function getResponses()
+    {
+        
+    }
+
+    public function toArray()
+    {
+        $array = parent::toArray();
+        $array['responses'] = $this->getResponses();
+        return $array;
+    }
 
 }
