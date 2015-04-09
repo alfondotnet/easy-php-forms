b73a68e8cf6621399e69071a5a6bd7697f466fc3
Alfonso Perez <alfon@ratslap.com>
2015-04-09 15:59:09 +0200

    form insertion beta

--- routers/submitform.router.php
+++ routers/submitform.router.php
@@ -2,35 +2,24 @@
 
 // /submitform/:id controller
 // Submit form controller
-// A form get submitted and you get redirected where the form says 
+// A form gets submitted and you get redirected where the form says 
 
-$app->post('/form/edit/:id/fields', function ($id) use ($app) {
+$app->post('/submitform/:id', function ($id) use ($app) {
     
     $c = array();
-    $allowed_field_names = Config::read('field_form_elements');
-    
-    $post_vars = $app->request()->post();
-    
-    // We iterate through id
-    foreach($post_vars as $key=>$var)
-    {
-        // as keys are name_id
-        $explode = explode('_', $key);
-        $id_field = array_pop($explode);
-        // column name can be placeholder or field_name
-        $column_name = implode('_',$explode);
+   
+    // We grab the form
+    $form = models\Form::find($id);
+    // We grab its responses
+    $response = new models\ModelBuilder;
+    $response::fromTable('responses_'.$id);
+    $response->form_id = $id;
 
-        // We try to avoid as possible possible Database mess
-        if(!in_array($column_name,$allowed_field_names))
-            continue;
+    $response->save();
 
-        // We edit the field
-        $field = models\Field::find($id_field);
-        $field->$column_name = $var;
-        $field->save();
-    }
+    die();
 
-    $app->redirect('/form/edit/'. $id);
+    $app->redirect($form->redirect);
 
 });
 
