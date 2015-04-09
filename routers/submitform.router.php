715a2b2b82024dc652a02cfa43e5bd9a177e04f8
Alfonso Perez <alfon@ratslap.com>
2015-04-09 16:50:38 +0200

    submitform beta

--- routers/submitform.router.php
+++ routers/submitform.router.php
@@ -4,16 +4,43 @@
 // Submit form controller
 // A form gets submitted and you get redirected where the form says 
 
+// TODO: Validate different field types
+
 $app->post('/submitform/:id', function ($id) use ($app) {
     
     $c = array();
    
+    $post_vars = $app->request()->post();
+    
     // We grab the form
     $form = models\Form::find($id);
-    // We grab its responses
+
+    // We grab its fields
+    $fields = $form->fields;
+
+    // We add a response
     $response = new models\ModelBuilder;
     $response::fromTable('responses_'.$id);
     $response->form_id = $id;
+
+    // We have to iterate through the form fields
+    foreach($post_vars as $key=>$var)
+    {
+        // We skip everything that is not a field
+        // as field lengths for example
+        if (substr($key, 0, 6) != 'field_') continue;
+
+        $id_field = end(explode('_',$key));
+
+        // We check that it's in the required format
+        if ((int)$id_field != $id_field) continue;
+
+        // if the field submitted exists within the form's scope
+        if (!$fields->find($id_field)) continue;
+
+        // Now we know $key is valid, we can assign it
+        $response->$key = $var;
+    }
 
     $response->save();
 
