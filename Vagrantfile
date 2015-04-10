2ee2d7201fce1f742dbc9ad342aaa92a68f266ec
Alfonso Perez <alfon@ratslap.com>
2015-04-10 16:30:11 +0200

    mail working and templating

--- Vagrantfile
+++ Vagrantfile
@@ -14,6 +14,7 @@ Vagrant.configure("2") do |config|
   config.vm.box = "forms"
   config.vm.box_url = "http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_ubuntu-12.04_chef-provisionerless.box"
   config.vm.network :forwarded_port, guest: 80, host: 9079
+  config.vm.network :forwarded_port, guest: 1080, host: 9080
   config.vm.provision :chef_solo do |chef|
     chef.cookbooks_path = ".provision"
     chef.add_recipe "apt"
--- routers/submitform.router.php
+++ routers/submitform.router.php
@@ -12,6 +12,7 @@ use lib\Config;
 $app->post('/submitform/:id', function ($id) use ($app) {
     
     $c = array();
+    $parameters = array();
    
     $post_vars = $app->request()->post();
     
@@ -29,6 +30,10 @@ $app->post('/submitform/:id', function ($id) use ($app) {
     $response = new models\ModelBuilder;
     $response::fromTable('responses_'.$id);
     $response->form_id = $id;
+
+    // This is the array containing the form fields and their values
+    // that will be passed to the twig template for them to be sent by e-mail
+    $parameters['fields'] = array();
 
     // We have to iterate through the form fields
     foreach($post_vars as $key=>$var)
@@ -51,7 +56,15 @@ $app->post('/submitform/:id', function ($id) use ($app) {
         // We use a helper to assist us in the different use cases we can 
         // encounter where the type of input we want to create gives as the value in a format
         // different than we want to store (e.g. Checkbox gives 'on' and we want to store 1)
-        $response->$key = DbConversor::convert($form_field->getTypeString(), $var);
+        $value_inserted = DbConversor::convert($form_field->getTypeString(), $var);
+        
+        $response->$key = $value_inserted;
+
+        // We also add it to our parameters array for it to be send to the contact by 
+        // e-mail
+        $parameters['fields'][$key] = $value_inserted;
+        // And the field names
+        $parameters['field_names'][$key] = $form_field->field_name; 
     }
 
     $response->save();
@@ -62,9 +75,12 @@ $app->post('/submitform/:id', function ($id) use ($app) {
     $transport = Swift_MailTransport::newInstance(); // Create the Mailer using your created Transport 
     $mailer = Swift_Mailer::newInstance($transport);
 
-    $parameters = array('form' => $form);
+    $parameters['form'] = $form;
 
     foreach ($contacts as $contact) {
+
+        // Contact to be accessible from the template
+        $parameters['contact'] = $contact;
 
         $generator = new Email($twig);
         $message = $generator->getMessage(Config::read('email_template'), $parameters);
@@ -72,8 +88,10 @@ $app->post('/submitform/:id', function ($id) use ($app) {
         $message->setFrom(Config::read('email_from'));
 
         $mailer->send($message);
+
     }
 
+    die();
 
     $app->redirect($form->redirect);
 
