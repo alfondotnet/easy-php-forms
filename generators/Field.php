793b9790e9e8e9eca1d4891cb6adcd97e8bb9120
Alfonso Perez <alfon@ratslap.com>
2015-04-10 16:38:34 +0200

    emai field working

--- generators/Field.php
+++ generators/Field.php
@@ -12,15 +12,20 @@ class Field {
         return '<input type="text" name="'.$name.'" class="form-control" placeholder="'.$placeholder.'" value="'. $default .'" '.$html5_attrs.'/>';
     }
 
+    public function email($name, $default='', $placeholder='', $html5_attrs='')
+    {
+        return '<input type="email" name="'.$name.'" class="form-control" placeholder="'.$placeholder.'" value="'.$default.'" '.$html5_attrs.'/>';
+    }
+
     public function textarea($name, $default='', $placeholder='', $html5_attrs='')
     {
-        return '<textarea name="'. $name .'">'. $default .'</textarea>';
+        return '<textarea class="form-control" name="'. $name .'">'. $default .'</textarea>';
     }
 
     public function checkbox($name, $checked=false, $placeholder='', $html5_attrs='')
     {
         $checked_attr = $checked ? 'checked="checked"' : '';
-        return '<input type="checkbox" name="'. $name .'" '.$checked_attr.'/>';
+        return '<input class="form-control" type="checkbox" name="'. $name .'" '.$checked_attr.'/>';
     }
 
 }
--- www/js/functions_main.js
+++ www/js/functions_main.js
@@ -1,3 +1,9 @@
+/*
+* Add fields to the "new form" form
+* TODO: get types from BD in the Slim controller and pass them as a JSON to this function, so changing them
+* in the database changes them here
+*/
+
 function addFields()
 {
     // Number of fields
@@ -9,7 +15,7 @@ function addFields()
 
     for (i = 0; i < parseInt(numberFields); i++)
     {
-        var field_html = '<h4>Field '+ (i+1) +'</h4><div class="form-group"><label class="col-sm-2 control-label" for="field_type_'+ (i+1) +'">Type of the field</label><div class="col-sm-10"><select name="field_type_'+ (i+1) +'"><option value="1">text</option><option value="2">textarea</option><option value="3">checkbox</option></select></div>';
+        var field_html = '<h4>Field '+ (i+1) +'</h4><div class="form-group"><label class="col-sm-2 control-label" for="field_type_'+ (i+1) +'">Type of the field</label><div class="col-sm-10"><select name="field_type_'+ (i+1) +'"><option value="1">text</option><option value="2">textarea</option><option value="3">checkbox</option><option value="4">email</option></select></div>';
         field_html += '<label class="col-sm-2 control-label" for="field_length_'+ (i+1) +'">Length of the field (only if text)</label><div class="col-sm-10"><input type="text" name="field_length_'+ (i+1) +'" value="150" /></div></div>';
         
         $('#field_dynamic_container').append(field_html);
